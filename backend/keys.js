6d945e49ce47f9a89cd73b30564d381f3d377d01
knightfury16 <suhaibahmmad1493@gmail.com>
2024-03-31 09:59:24 +0600

    created rabbitmq connection

--- backend/keys.js
+++ backend/keys.js
@@ -1,9 +1,14 @@
 module.exports = {
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-    pgHost: process.env.PG_HOST,
-    pgUser: process.env.PG_USER,
-    pgDatabase: process.env.PG_DATABASE,
-    pgPassword: process.env.PG_PASSWORD,
-    pgPort: process.env.PG_PORT
-}
+  redisHost: process.env.REDIS_HOST || "localhost",
+  redisPort: process.env.REDIS_PORT || 6379,
+  pgHost: process.env.PG_HOST || "localhost",
+  pgUser: process.env.PG_USER || "postgres",
+  pgDatabase: process.env.PG_DATABASE || "postgres",
+  pgPassword: process.env.PG_PASSWORD || "admin",
+  pgPort: process.env.PG_PORT || 5432,
+  amqpHost: process.env.AMQP_HOST || "localhost",
+  amqpPassword: process.env.AMQP_PASSWORD || "admin",
+  amqpUser: process.env.AMQP_USER || "admin",
+  amqpPort: process.env.AMQP_PORT || "5672",
+};
+
---
+++ backend/rabbitConnection.js
@@ -0,0 +1,17 @@
+const amqplib = require("amqplib");
+const { amqpHost, amqpPort, amqpUser, amqpPassword } = require("./keys.js");
+
+const amqpUri = `amqp://${amqpUser}:${amqpPassword}@${amqpHost}:${amqpPort}`;
+
+async function createRabbitConnection() {
+  try {
+    const connection = await amqplib.connect(amqpUri);
+    console.log("Connected to rabbitmq successfully");
+    return connection;
+  } catch (error) {
+    console.error("Error connecting to rabbitmq");
+    throw error;
+  }
+}
+
+module.exports = { createRabbitConnection };
