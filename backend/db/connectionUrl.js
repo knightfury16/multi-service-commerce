6a202da425f2f211f9a2e2a13051338d6045d493
knightfury16 <suhaibknight@gmail.com>
2024-05-27 17:40:42 +0600

    set host dynamically

--- backend/db/connectionUrl.js
+++ backend/db/connectionUrl.js
@@ -4,9 +4,7 @@ const username = process.env.PG_USER || 'postgres';
 const password = process.env.PG_PASSWORD || '1234554321';
 const databaseName = process.env.PG_DATABASE || 'cds_test';
 const port = process.env.PG_PORT || 5432;
-const isDocker = process.env.PG_HOST !== undefined ? 'true' : 'false'; // Optional check for Docker environment (if applicable)
-
-const host = isDocker === 'true' ? 'postgres' : 'localhost'; // Use 'postgres' in Docker, 'localhost' otherwise
+const host = process.env.PG_HOST !== undefined ? process.env.PG_HOST : 'localhost'; // Optional check for Docker environment (if applicable)
 
 const connectionUrl = `postgresql://${username}:${password}@${host}:${port}/${databaseName}`;
 
