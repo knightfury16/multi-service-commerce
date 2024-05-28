cf9e8b81dc61b3019da17009bde19b975e08cdf5
knightfury16 <suhaibknight@gmail.com>
2024-05-28 17:04:09 +0600

    changed env to run frontend locally for development

--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -1,2 +1,4 @@
-// config.ts
-export const BaseUrl: string = "/api";
+// Access env variable from import.meta.env and prefix the variable using VITE_....
+let url: string = import.meta.env.VITE_HOST ? "/api" : "http://localhost:3000/api"
+console.log("Base Url: ", url);
+export const BaseUrl: string = url;
